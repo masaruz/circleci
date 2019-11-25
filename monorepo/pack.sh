8c2f3154c2536fd264067c60c7bf1600c4fc12d5
Stamp <stampz74@gmail.com>
2019-11-25 18:27:16 +0700

    use yarn workspaces to pack all packages

--- monorepo/pack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=monorepo
-PWD=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
-
-echo "Start generate script from $PWD$DIR"
-
-circleci config pack $PWD$DIR/src > $PWD$DIR/config.yml
-circleci orb validate $PWD$DIR/config.yml 
